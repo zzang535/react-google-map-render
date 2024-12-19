fbb63055a9ea1bf6a4bc3cd5267cc67321113b53
zzang535 <zzang535@gmail.com>
2024-12-19 10:48:50 +0900

    feat: render google map comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import GoogleMap from '@zzang535/react-google-map';
+import { GoogleMap, type GoogleMapProps } from '@zzang535/react-google-map';
 
 export default function Home() {
   return (
-    <>
-      <GoogleMap />
-    </>
+    <div style={{ height: '100vh', width: '100vw', border: '10px solid white' }} >
+      <GoogleMap
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}
+        center={{
+            lat: 37.480392,
+            lng: 126.998787,
+        }}
+        zoom={16}
+        markers={[{
+            lat: 37.480392,
+            lng: 126.998787,
+        }]}
+        mapId={'map-id'}
+      />
+    </div>
   );
 }
