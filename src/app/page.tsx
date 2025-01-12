3c023be80c9c82c97792394b479013fe08f307c3
zzang535 <zzang535@gmail.com>
2025-01-12 19:38:35 +0900

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
-import { GoogleMap, type GoogleMapProps } from '@zzang535/react-google-map';
+import { GoogleMap, type GoogleMapProps } from "@zzang535/react-google-map";
 
 export default function Home() {
+  const mapProps: GoogleMapProps = {
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!,
+    center: {
+      lat: 37.480392,
+      lng: 126.998787,
+    },
+    zoom: 16,
+    markers: [
+      {
+        lat: 37.480392,
+        lng: 126.998787,
+      },
+    ],
+    mapId: "map-id",
+  };
+
   return (
-    <div style={{ height: '100vh', width: '100vw', border: '10px solid white' }} >
-      <GoogleMap
-        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}
-        center={{
-            lat: 37.480392,
-            lng: 126.998787,
-        }}
-        zoom={16}
-        markers={[{
-            lat: 37.480392,
-            lng: 126.998787,
-        }]}
-        mapId={'map-id'}
-      />
+    <div style={{ height: "100vh", width: "100vw", border: "10px solid pink" }}>
+      <GoogleMap {...mapProps} />
     </div>
   );
 }
